cc2847d9b89c9592a9cd5152ac3510af7d7ad7c3
Dencel Babu <dencelbabu@gmail.com>
2025-12-17 18:19:03 +0530

    Hide console by default and add --debug flag

--- BudsHeadTrackingBridge/Program.cs
+++ BudsHeadTrackingBridge/Program.cs
@@ -4,8 +4,17 @@ namespace BudsHeadTrackingBridge;
 
 public static class Program
 {
+    [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+    private static extern bool AllocConsole();
+
     public static void Main(string[] args)
     {
+        if (args.Contains("--debug"))
+        {
+            AllocConsole();
+            System.Console.WriteLine("[DEBUG] Console allocated.");
+        }
+
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
     }
